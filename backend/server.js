4564235aeec9994d12df09750ef1451a9844e605
Shubhanjali <shubhanjali32@gmail.com>
2026-03-20 19:01:32 +0530

    add: create render.yml for backend service configuration

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // ── Core Middleware ───────────────────────────────────────────
 app.use(helmet())                                    // Security
-app.use(cors({ origin: "http://localhost:5173" }))   
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))   
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
